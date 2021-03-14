54d5d3d137d1623a81e125ad1d9edcc68d8859fd
MyNameIsTrez <welfje@gmail.com>
2021-03-14 05:04:58 +0100

    Refactored code into functions.

--- split_into_icons.py
+++ split_into_icons.py
@@ -1,23 +1,28 @@
 import os, pathlib, shutil
 
 
+def create_sprite_sheet_parent_dir(icons_png_path):
+	if not os.path.isdir(icons_png_path.parent):
+		os.makedirs(icons_png_path.parent)
+
+
+def create_icons_dir(icons_png_path):
+	# Make a folder which will contain the icons of individual items.
+	subicons_folder_path = str(icons_png_path.parent / icons_png_path.stem) + "_icons"
+	if not os.path.isdir(subicons_folder_path):
+		os.mkdir(subicons_folder_path)
+
+
 if __name__ == "__main__":
 	mods_dir = "mods" # Watch out with this path, you can accidentally delete everything on your computer with it!
 	icons_dir = "icons"
 
 	for root, subdirs, png_files in os.walk(mods_dir):
 		for png_filename in png_files:
-			p = pathlib.Path(root)
-			
-			png_path = p / png_filename
+			png_path = pathlib.Path(root) / png_filename
 
-			# Removes the mods_dir part and replaces it with icons_dir.
+			# Removes the mods_dir folder at the beginning and replaces it with icons_dir.
 			icons_png_path = icons_dir / pathlib.Path(*png_path.parts[1:])
 
-			if not os.path.isdir(icons_png_path.parent):
-				os.makedirs(icons_png_path.parent)
-
-			# Make a folder which will contain the icons of individual items.
-			subicons_folder_path = str(icons_png_path.parent / icons_png_path.stem) + "_icons"
-			if not os.path.isdir(subicons_folder_path):
-				os.mkdir(subicons_folder_path)
+			create_sprite_sheet_parent_dir(icons_png_path)
+			create_icons_dir(icons_png_path)
