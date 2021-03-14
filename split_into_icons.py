67c6abc754f71ab8daa7eb2d4f439d73d633b286
MyNameIsTrez <welfje@gmail.com>
2021-03-14 05:09:09 +0100

    Refactoring.

--- split_into_icons.py
+++ split_into_icons.py
@@ -1,14 +1,14 @@
 import os, pathlib, shutil
 
 
-def create_sprite_sheet_parent_dir(icons_png_path):
-	if not os.path.isdir(icons_png_path.parent):
-		os.makedirs(icons_png_path.parent)
+def create_sprite_sheet_parent_dir(sprite_sheet_path):
+	if not os.path.isdir(sprite_sheet_path.parent):
+		os.makedirs(sprite_sheet_path.parent)
 
 
-def create_icons_dir(icons_png_path):
+def create_icons_dir(sprite_sheet_path):
 	# Make a folder which will contain the icons of individual items.
-	subicons_folder_path = str(icons_png_path.parent / icons_png_path.stem) + "_icons"
+	subicons_folder_path = str(sprite_sheet_path.parent / sprite_sheet_path.stem) + "_icons"
 	if not os.path.isdir(subicons_folder_path):
 		os.mkdir(subicons_folder_path)
 
@@ -19,10 +19,13 @@ if __name__ == "__main__":
 
 	for root, subdirs, png_files in os.walk(mods_dir):
 		for png_filename in png_files:
-			png_path = pathlib.Path(root) / png_filename
+			png_sprite_sheet_path = pathlib.Path(root) / png_filename
 
 			# Removes the mods_dir folder at the beginning and replaces it with icons_dir.
-			icons_png_path = icons_dir / pathlib.Path(*png_path.parts[1:])
+			sprite_sheet_path = icons_dir / pathlib.Path(*png_sprite_sheet_path.parts[1:])
 
-			create_sprite_sheet_parent_dir(icons_png_path)
-			create_icons_dir(icons_png_path)
+			create_sprite_sheet_parent_dir(sprite_sheet_path)
+			create_icons_dir(sprite_sheet_path)
+
+			# TODO: sprite_sheet() should be a generator.
+			# for icon_path in sprite_sheet():
