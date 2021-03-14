a7e0c22399d72a916df4d0e08fa611b52d016280
MyNameIsTrez <welfje@gmail.com>
2021-03-14 05:21:44 +0100

    Refactoring.

--- split_into_icons.py
+++ split_into_icons.py
@@ -1,16 +1,31 @@
 import os, pathlib, shutil
 
 
-def create_sprite_sheet_parent_dir(sprite_sheet_path):
-	if not os.path.isdir(sprite_sheet_path.parent):
-		os.makedirs(sprite_sheet_path.parent)
+def create_sprite_sheet_parent_dir(sprite_sheet_path_parent):
+	if not os.path.isdir(sprite_sheet_path_parent):
+		os.makedirs(sprite_sheet_path_parent)
 
 
-def create_icons_dir(sprite_sheet_path):
+def create_icons_dir(sprite_sheet_path_parent, sprite_sheet_path):
 	# Make a folder which will contain the icons of individual items.
-	subicons_folder_path = str(sprite_sheet_path.parent / sprite_sheet_path.stem) + "_icons"
+	subicons_folder_path = str(sprite_sheet_path_parent / sprite_sheet_path.stem) + "_icons"
 	if not os.path.isdir(subicons_folder_path):
 		os.mkdir(subicons_folder_path)
+
+
+def create_icons(old_sprite_sheet_path, sprite_sheet_path_parent):
+	i = 0
+	for icon_image in get_icon_images(old_sprite_sheet_path):
+		print(icon_image)
+
+		icon_path = str(sprite_sheet_path_parent) + "_" + str(i)
+		print(icon_path)
+		
+		i += 1
+
+
+def get_icon_images(old_sprite_sheet_path):
+	yield "foo"
 
 
 if __name__ == "__main__":
@@ -19,13 +34,12 @@ if __name__ == "__main__":
 
 	for root, subdirs, png_files in os.walk(mods_dir):
 		for png_filename in png_files:
-			png_sprite_sheet_path = pathlib.Path(root) / png_filename
+			old_sprite_sheet_path = pathlib.Path(root) / png_filename
 
 			# Removes the mods_dir folder at the beginning and replaces it with icons_dir.
-			sprite_sheet_path = icons_dir / pathlib.Path(*png_sprite_sheet_path.parts[1:])
+			sprite_sheet_path = icons_dir / pathlib.Path(*old_sprite_sheet_path.parts[1:])
+			sprite_sheet_path_parent = sprite_sheet_path.parent
 
-			create_sprite_sheet_parent_dir(sprite_sheet_path)
-			create_icons_dir(sprite_sheet_path)
-
-			# TODO: sprite_sheet() should be a generator.
-			# for icon_path in sprite_sheet():
+			create_sprite_sheet_parent_dir(sprite_sheet_path_parent)
+			create_icons_dir(sprite_sheet_path_parent, sprite_sheet_path)
+			create_icons(old_sprite_sheet_path, sprite_sheet_path_parent)
